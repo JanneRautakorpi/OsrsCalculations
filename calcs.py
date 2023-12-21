f828a9b6ead0a44e398687bb35def04e41392ae1
Janne Rautakorpi <112503143+JanneRautakorpi@users.noreply.github.com>
2023-12-22 00:05:54 +0200

    Update calcs.py

--- calcs.py
+++ calcs.py
@@ -4,13 +4,27 @@ import math
 strengthLevel = 99
 totalStrength = strengthLevel + 19
 attackLevel = 99
+totalAttack = attackLevel + 19
 
-prayerMultipliers = {
+AttDefRolls = {
+    "AttRoll":0,
+    "DefRoll":0
+}
+
+prayerStr = {
     "burst":1.05,
     "superhuman":1.10,
     "ultimate":1.15,
     "chivalry":1.18,
     "piety":1.23
+}
+
+prayerAtt = {
+    "clarity":1.05,
+    "improved":1.10,
+    "incredible":1.15,
+    "chivalry":1.15,
+    "piety":1.20
 }
 
 Style = {
@@ -28,10 +42,22 @@ class NPC:
         self.slashD = slashD
         self.crushD = crushD
 
+    def calcDefRoll(self):
+        if (Style["stab"] == True):
+            defStyle = self.stabD
+        elif (Style["slash"] == True):
+            defStyle = self.slashD
+        else:
+            defStyle = self.crushD
+        
+        defRoll = (self.defence + 9) * (defStyle + 64)
+        AttDefRolls["DefRoll"] = defRoll
+        
 class Player:
     def __init__(self, attBonus, strLevel,
                  strBonus, prayer, meleeVoid,
-                 effStr, attStyle, maxHit):
+                 effStr, attStyle, maxHit,
+                 effAtt):
         self.attBonus = attBonus
         self.strLevel = strLevel
         self.strBonus = strBonus
@@ -39,11 +65,12 @@ class Player:
         self.meleeVoid = meleeVoid
         self.effStr = effStr
         self.attStyle = attStyle
-        self.maxHit = maxHit
+        self.maxHit = maxHit,
+        self.effAtt = effAtt
         Style[attStyle] = True
 
     def calcEffStr(self):
-        prayerBonus = prayerMultipliers[self.prayer]
+        prayerBonus = prayerStr[self.prayer]
 
         tmp = math.floor(self.strLevel * prayerBonus) + 3
         tmp += 8
@@ -51,6 +78,15 @@ class Player:
             tmp *= 1.1
         tmp = math.floor(tmp)
         self.effStr = tmp
+
+    def calcEffAtt(self):
+        prayerBonus = prayerAtt[self.prayer]
+
+        att = math.floor(totalAttack * prayerBonus)
+        att += 8
+        if (self.meleeVoid is not None):
+            att *= 1.1
+        self.effAtt = math.floor(att)
 
     def calcMaxHit(self):
         multiplier = 1 #1 because of regular monster. Higher if undead, on-task etc..
@@ -62,15 +98,27 @@ class Player:
         max = math.floor(max)
         self.maxHit = max
 
+    def calcAttRoll(self):
+        attackMult = 1 #1 because regular monster
+        tmpAtt = self.effAtt * (self.attBonus + 64)
+        tmpAtt *= attackMult
+        AttDefRolls["AttRoll"] = math.floor(tmpAtt)
+
 def main():
+    attacker = Player(156, totalStrength, 158,
+                      "piety", None, 0, "slash", 0,
+                      0)
+    attacker.calcEffStr()
+    attacker.calcMaxHit()
+    attacker.calcEffAtt()
+    attacker.calcAttRoll()
+
     # monster = General Graardor
     monster = NPC(255, 250, 90,
                   90, 90)
-    attacker = Player(156, totalStrength, 158,
-                      "piety", None, 0, "slash", 0)
-    attacker.calcEffStr()
-    attacker.calcMaxHit()
-    print(attacker.maxHit)
+    monster.calcDefRoll()
+    
+
 
 if __name__ == "__main__":
     main()
